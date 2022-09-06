56bc68658998d29502189aaec995576f8a4d1a6b
đttha <dothithuha140@gmail.com>
2022-09-06 22:32:41 +0700

    complete cart

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ app.get('/api/products/slug/:slug', (req, res) => {
         res.status(404).send({ message: 'Sản phẩm không tồn tại' })
     }
 })
-
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x.id === req.params.id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.status(404).send({ message: 'Sản phẩm không tồn tại' })
+    }
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () =>{
     console.log(`serve at http://localhost:${port}`)
