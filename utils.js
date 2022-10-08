3a1101fab80fff34e88280324e17b5effccfdbaa
đttha <dothithuha140@gmail.com>
2022-10-08 22:58:43 +0700

    add admin

--- utils.js
+++ utils.js
@@ -36,3 +36,11 @@ export const isAuth = (req, res, next) => {
         res.status(401).send({ message: 'Không có Token' })
     }
 }
+
+export const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401).send({ message: 'Invalid Admin Token' });
+    }
+};
