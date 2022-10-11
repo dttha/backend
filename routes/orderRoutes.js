acaa7f36869ec48c685be7aa8d9d2f76d58c625d
đttha <dothithuha140@gmail.com>
2022-10-11 22:31:39 +0700

    update

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../model/orderModel.js';
-import { isAuth } from '../utils.js';
+import { isAdmin, isAuth, mailgun, payOrderEmailTemplate } from '../utils.js';
 
 const orderRouter = express.Router();
+orderRouter.get(
+    '/',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+        const orders = await Order.find().populate('user', 'name');
+        res.send(orders);
+    })
+);
+
 orderRouter.post(
     '/',
     isAuth,
@@ -49,7 +59,10 @@ orderRouter.put(
     '/:id/pay',
     isAuth,
     expressAsyncHandler(async (req, res) => {
-        const order = await Order.findById(req.params.id)
+        const order = await Order.findById(req.params.id).populate(
+            'user',
+            'email name'
+        );
         if (order) {
             order.isPaid = true;
             order.paidAt = Date.now();
@@ -58,13 +71,31 @@ orderRouter.put(
                 status: req.body.status,
                 update_time: req.body.update_time,
                 email_address: req.body.email_address,
-            }
-            const updateOrder = await Order.save()
-            res.send({ message: 'Đơn hàng đã thanh toán', order: updateOrder })
-        }
-        else {
-            res.status(404).send({ message: 'Đơn hàng không tồn tại' })
+            };
+
+            const updatedOrder = await order.save();
+            mailgun()
+                .messages()
+                .send(
+                    {
+                        from: 'Amazona <amazona@mg.yourdomain.com>',
+                        to: `${order.user.name} <${order.user.email}>`,
+                        subject: `New order ${order._id}`,
+                        html: payOrderEmailTemplate(order),
+                    },
+                    (error, body) => {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log(body);
+                        }
+                    }
+            );
+
+            res.send({ message: 'Order Paid', order: updatedOrder });
+        } else {
+            res.status(404).send({ message: 'Order Not Found' });
         }
     })
-)
+);
 export default orderRouter;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,29 +9,6 @@ productRouter.get('/', async (req, res) => {
     const products = await Product.find()
     res.send(products)
 })
-productRouter.get(
-    '/categories',
-    expressAsyncHandler(async (req, res) => {
-        const categories = await Product.find().distinct('category');
-        res.send(categories);
-    })
-);
-productRouter.get('/slug/:slug', async (req, res) => {
-    const product = await Product.findOne({ slug: req.params.slug })
-    if (product) {
-        res.send(product)
-    } else {
-        res.status(404).send({ message: 'Sản phẩm không tồn tại' })
-    }
-})
-productRouter.get('/:_id', async (req, res) => {
-    const product = await Product.findById(req.params._id)
-    if (product) {
-        res.send(product)
-    } else {
-        res.status(404).send({ message: 'Sản phẩm không tồn tại' })
-    }
-})
 
 productRouter.post(
     '/',
@@ -55,35 +32,11 @@ productRouter.post(
     })
 );
 
-
-const PAGE_SIZE = 3;
-
-productRouter.get(
-    '/admin',
-    isAuth,
-    isAdmin,
-    expressAsyncHandler(async (req, res) => {
-        const { query } = req;
-        const page = query.page || 1;
-        const pageSize = query.pageSize || PAGE_SIZE;
-
-        const products = await Product.find()
-            .skip(pageSize * (page - 1))
-            .limit(pageSize);
-        const countProducts = await Product.countDocuments();
-        res.send({
-            products,
-            countProducts,
-            page,
-            pages: Math.ceil(countProducts / pageSize),
-        });
-    })
-);
-
 productRouter.get(
     '/search',
     expressAsyncHandler(async (req, res) => {
         const { query } = req;
+        console.log("🚀 ~ file: productRoutes.js ~ line 90 ~ expressAsyncHandler ~ query", query)
         const pageSize = query.pageSize || PAGE_SIZE;
         const page = query.page || 1;
         const category = query.category || '';
@@ -132,6 +85,7 @@ productRouter.get(
                             : order === 'newest'
                                 ? { createdAt: -1 }
                                 : { _id: -1 };
+        console.log("🚀 ~ file: productRoutes.js ~ line 128 ~ expressAsyncHandler ~ sortOrder", sortOrder)
 
         const products = await Product.find({
             ...queryFilter,
@@ -157,5 +111,58 @@ productRouter.get(
         });
     })
 );
+
+productRouter.get(
+    '/categories',
+    expressAsyncHandler(async (req, res) => {
+        console.log(123);
+        const categories = await Product.distinct('category');
+        console.log("🚀 ~ file: productRoutes.js ~ line 16 ~ expressAsyncHandler ~ categories", categories)
+        res.send(categories);
+    })
+);
+productRouter.get('/slug/:slug', async (req, res) => {
+    const product = await Product.findOne({ slug: req.params.slug })
+    if (product) {
+        res.send(product)
+    } else {
+        res.status(404).send({ message: 'Sản phẩm không tồn tại' })
+    }
+})
+productRouter.get('/:_id', async (req, res) => {
+    const product = await Product.findById(req.params._id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.status(404).send({ message: 'Sản phẩm không tồn tại' })
+    }
+})
+
+const PAGE_SIZE = 3;
+
+productRouter.get(
+    '/admin',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+        const { query } = req;
+        console.log("🚀 ~ file: productRoutes.js ~ line 69 ~ expressAsyncHandler ~ query", query)
+        const page = query.page || 1;
+        const pageSize = query.pageSize || PAGE_SIZE;
+
+        const products = await Product.find()
+            .skip(pageSize * (page - 1))
+            .limit(pageSize);
+        const countProducts = await Product.countDocuments();
+        res.send({
+            products,
+            countProducts,
+            page,
+            pages: Math.ceil(countProducts / pageSize),
+        });
+    })
+);
+
+
 
 export default productRouter
