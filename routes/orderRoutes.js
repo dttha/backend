ac67a601fb77743c88d08f89dc75d1a523a9c207
đttha <dothithuha140@gmail.com>
2023-01-09 20:47:01 +0700

    fix

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -62,7 +62,7 @@ orderRouter.get(
         const dailyOrders = await Order.aggregate([
             {
                 $group: {
-                    _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+                    _id: { $dateToString: { format: '%Y-%m', date: '$createdAt' } },
                     orders: { $sum: 1 },
                     sales: { $sum: '$totalPrice' },
                 },
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,7 +6,7 @@ import { isAuth, isAdmin } from '../utils.js';
 const productRouter = express.Router()
 
 productRouter.get('/', async (req, res) => {
-    const products = await Product.find()
+    const products = await Product.find().sort({ createdAt: -1 })
     res.send(products)
 })
 
